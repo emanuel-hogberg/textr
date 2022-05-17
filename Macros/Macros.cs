a9aa52c5b69b8f0faffbeecc892140eefcb31b2c
metavariabel <emanuel.hogberg@gmail.com>
2022-05-17 08:33:07 +0200

    added macros

---
+++ Macros/Macros.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using emanuel.Transforms;
+
+namespace emanuel.Macros
+{
+    public class Macros
+    {
+        List<ITransform> transforms = new List<ITransform>();
+
+        public Macros AddTransform(ITransform transform)
+        {
+            transforms.Add(transform);
+            return this;
+        }
+
+        public Macros AddTransforms(List<ITransform> transforms)
+        {
+            this.transforms.AddRange(transforms);
+            return this;
+        }
+
+        public List<ITransform> ToList() => transforms;
+    }
+}
